9ae5cb81e7fed3b50ac7cfd04629752c1f09a4fb
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 18:35:45 +0000

    added logic to return number of days in month

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,46 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(1, -2020));
+    }
 
+    public static boolean isLeapYear(int year) {
+
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                return true;
+            } else {
+                return year % 400 == 0;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 2 -> {
+                if (isLeapYear(year)) {
+                    yield  29;
+                } else {
+                    yield 28;
+                }
+            }
+            case 4, 6, 9, 11 -> 30;
+            default -> -1;
+        };
     }
 }
